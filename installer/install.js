bdafb95592a930bfd2df41210f9a15b17b031282
Christopher Shanti Burkey <entermedia-community@entermediasoftware.com>
2013-08-06 12:56:41 -0400

    Moved back to groovy

--- installer/install.js
+++ installer/install.js
@@ -4,12 +4,11 @@ importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
 var root = moduleManager.getBean("root").getAbsolutePath();
-var eml = root + "/showcase";
 var tmp = root + "/WEB-INF/temp/upgrade/";
 
 
 var files = new FileUtils();
-log.add("4. UPGRADE BASE DIR");
-files.deleteAll( root + "/WEB-INF/base/showcase");
-files.copyFiles( tmp + "/showcase", root + "/WEB-INF/base/showcase/");
+log.add("UPGRADE BASE DIR");
+files.deleteAll( root + "/WEB-INF/base/aws");
+files.copyFiles( tmp + "/WEB-INF/base/aws", root + "/WEB-INF/base/aws");
 
