e3d71d22938b46c6fd5dbb56531df5f86043d38e
Ian <ian@netevolved.com>
2016-09-23 11:51:42 -0400

    fix installer

--- etc/install.js
+++ etc/install.js
@@ -5,21 +5,21 @@ importPackage( Packages.com.openedit.modules.update );
 
 var name = "extension-aws";
 
-var war = "http://dev.entermediasoftware.com/jenkins/job/@BRANCH@" + name + "/lastSuccessfulBuild/artifact/deploy/" + name + ".zip";
+var war = "http://dev.entermediasoftware.com/jenkins/job/@BRANCH@_" + name + "/lastSuccessfulBuild/artifact/deploy/" + name + ".zip";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
 var web = root + "/WEB-INF";
 var tmp = web + "/tmp";
 
-log.add("1. GET THE LATEST WAR FILE");
+log.info("1. GET THE LATEST WAR FILE");
 var downloader = new Downloader();
 downloader.download( war, tmp + "/" + name + ".zip");
 
-log.add("2. UNZIP WAR FILE");
+log.info("2. UNZIP WAR FILE");
 var unziper = new ZipUtil();
 unziper.unzip(  tmp + "/" + name + ".zip",  tmp + "/unzip/" );
 
-log.add("3. Copy Over Site " + tmp + "/unzip/" + " " + "to " + root);
+log.info("3. Copy Over Site " + tmp + "/unzip/" + " " + "to " + root);
 var files = new FileUtils();
 files.deleteMatch( web + "/lib/@BRANCH@extension-aws*.jar");
 files.deleteMatch( web + "/lib/aws-java*.jar");
@@ -39,7 +39,7 @@ files.copyFileByMatch( tmp + "/lib/@BRANCH@extension-aws*.jar", web + "/lib/");
 
 
 
-log.add("5. CLEAN UP");
+log.info("5. CLEAN UP");
 files.deleteAll(tmp);
 
-log.add("6. UPGRADE COMPLETED");
+log.info("6. UPGRADE COMPLETED");
