63a4e804ec61beeff0d1976a1e18e05988cc41e7
Ian <ian@netevolved.com>
2018-05-16 16:13:43 -0400

    Amazon updates

--- src/org/entermediadb/amazon/amazonpublisher.java
+++ src/org/entermediadb/amazon/amazonpublisher.java
@@ -1,51 +1,49 @@
 package org.entermediadb.amazon;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.entermediadb.asset.Asset;
 import org.entermediadb.asset.MediaArchive;
 import org.entermediadb.asset.publishing.BasePublisher;
 import org.entermediadb.asset.publishing.PublishResult;
 import org.openedit.Data;
+import org.openedit.page.Page;
+import org.openedit.repository.filesystem.FileItem;
 
 public class amazonpublisher extends BasePublisher 
 {
 
-	@Override
-	public PublishResult publish(MediaArchive inMediaArchive, Asset inAsset, Data inPublishRequest, Data inDestination, Data inPreset)
+	private static final Log log = LogFactory.getLog(amazonpublisher.class);
+
+	public PublishResult publish(MediaArchive mediaArchive,Asset asset, Data inPublishRequest,  Data destination, Data inPreset)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		S3Repository repo = (S3Repository) mediaArchive.getModuleManager().getBean("S3Repository");
+		log.info("Publish asset to Amazon ${asset} for on server: ${destination}" );
+
+		repo.setBucket(destination.get("bucket"));
+		repo.setAccessKey(destination.get("accesskey"));
+		repo.setSecretKey(destination.get("secretkey"));
+
+		//repo.setExternalPath(getRoot().getAbsolutePath() + "/WEB-INF/s3cache" );
+		//repo.setPath("/WEB-INF/data/test/originals/bucket1");
+		
+		
+		PublishResult result = new PublishResult();
+		//open the file and send it
+		Page inputpage = findInputPage(mediaArchive,asset,inPreset);
+		String exportname = inPublishRequest.get("exportname");
+		if( !exportname.startsWith("/"))
+		{
+			exportname = "/" + exportname;
+		}
+		FileItem item = new FileItem();
+		item.setPath( exportname);
+		item.setAbsolutePath(inputpage.getContentItem().getAbsolutePath());
+
+		repo.put(item); //copy the file
+		log.info("published  ${exportname} to Amazon s3");
+		result.setComplete(true);
+		return result;
 	}
-//	private static final Log log = LogFactory.getLog(amazonpublisher.class);
-//
-//	public PublishResult publish(MediaArchive mediaArchive,Asset asset, Data inPublishRequest,  Data destination, Data inPreset)
-//	{
-//		def repo = mediaArchive.getModuleManager().getBean("S3Repository");
-//		log.info("Publish asset to Amazon ${asset} for on server: ${destination}" );
-//
-//		repo.setBucket(destination.bucket);
-//		repo.setAccessKey(destination.accesskey);
-//		repo.setSecretKey(destination.secretkey);
-//
-//		//repo.setExternalPath(getRoot().getAbsolutePath() + "/WEB-INF/s3cache" );
-//		//repo.setPath("/WEB-INF/data/test/originals/bucket1");
-//		
-//		
-//		PublishResult result = new PublishResult();
-//		//open the file and send it
-//		Page inputpage = findInputPage(mediaArchive,asset,inPreset);
-//		String exportname = inPublishRequest.get("exportname");
-//		if( !exportname.startsWith("/"))
-//		{
-//			exportname = "/" + exportname;
-//		}
-//		FileItem item = new FileItem();
-//		item.setPath( exportname);
-//		item.setAbsolutePath(inputpage.getContentItem().getAbsolutePath());
-//
-//		repo.put(item); //copy the file
-//		log.info("published  ${exportname} to Amazon s3");
-//		result.setComplete(true);
-//		return result;
-//	}
 }
 
